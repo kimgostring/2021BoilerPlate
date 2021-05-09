74841a68ad75324e79995ac42d5a77ce4db6d078
kimgahyeon <kimgostring@naver.com>
2021-05-09 22:22:36 +0900

    노드 리액트 기초 강의 #21 데이터 Flow & Axios

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import axios from "axios";
 
 function LandingPage() {
+    // LandingPage에 들어오자 마자 실행됨
+    useEffect(() => {
+        // get request server에 보냄 (port 3000으로)
+        axios.get("/api/hello")
+        // 요청을 통해 server로부터 받아온 response를 console에 출력
+        .then(res => console.log(res.data));
+    }, []);
+    
     return (
         <div>
             LandingPage
         </div>
-    )
+    );
 }
 
 export default LandingPage
