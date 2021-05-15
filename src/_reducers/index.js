26f8ad93e5cf4b88d1e1062f8fd6777df6178ac1
kimgahyeon <kimgostring@naver.com>
2021-05-16 00:03:08 +0900

    노드 리액트 기초 강의 #27 Redux UP

---
+++ src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import user from './user_reducer'; // 아직 생성 X
+
+const rootReducer = combineReducers({
+    // user,
+});
+
+export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunx from 'redux-thunk';
+import Reducer from './_reducers/index';
+
+const createStoreWithMiddle = applyMiddleware(promiseMiddleware, ReduxThunx)(createStore);
+// 아직 함수 실행 X, 실행을 위해서는 createStore의 인자 넣어주어야 함
+// 미들웨어 적용된 createStore을 만들기만 함
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={ createStoreWithMiddle(Reducer, 
+      // 앞뒤로 __ 꼭 추가해주어야 함
+      window.__REDUX_DEVTOOLS_EXTENSION__ && 
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    ) /* Redux Devtools store enhancer 연결 */ } 
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
